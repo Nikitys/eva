3de5f119af80a8c22f8e87a2a68f0b44cc0fdaed
Никита Толкач <nikitatolkac@MacBook-Pro-Nikita.local>
2017-12-14 19:13:59 +0300

    eva no adaptiv

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,6 +28,37 @@ $(document).ready(function() {
         arrows: false,
         dots: false,
         slidesToShow: 5,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
     });
     $(document).on('click', '.slider-list-arrow--prev', function () {
         slider.slick('slickPrev');
@@ -37,5 +68,37 @@ $(document).ready(function() {
         slider.slick('slickNext');
         return false;
     });
+    $(document).on('click','.hamburger', function () {
+        if ($(this).hasClass('is-active')){
+            $('html').removeClass('no-scroll');
+        } else {
+            $('html').addClass('no-scroll');
+        }
+        $(this).toggleClass('is-active');
+        $('.mob-menu').toggleClass('mob-menu--active');
 
+    });
+    $(document).on('click','.items-main-left-block__title', function () {
+        $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+        $(this).parents('.items-main-left-block').find('.items-main-left-list').slideToggle();
+    });
+    $(document).on('click','.items-main-right-top-title', function () {
+        var classLink = $(this).attr('data-link')
+        if ($('.' + classLink).hasClass('active')){
+            $('html').removeClass('no-scroll');
+        } else {
+            $('html').addClass('no-scroll');
+        }
+        $('.' + classLink).toggleClass('active');
+
+    });
+    $(document).on('click','.items-main-right-but', function () {
+        $(this).parents('.items-main--full-page-menu').removeClass('active');
+        $('html').removeClass('no-scroll');
+        return false;
+    });
+    $(document).on('click','.items-main-right-top-center-close', function () {
+        $(this).parents('.items-main--full-page-menu').removeClass('active');
+        $('html').removeClass('no-scroll');
+    });
 });
